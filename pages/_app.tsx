02084a147544f4db38e05909402ea43e6d5628a8
devil1321 <s.dominik1321@gmail.com>
2023-10-13 21:14:45 +0200

    fix init state redux in _app.tsx connected all app using redux.inner component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from 'react';
-import Redux from './_redux'
 import { AppProps } from 'next/app';
 import '../styles/global.css'
 
 import store from '../controller/store'
 import { Provider } from 'react-redux';
 
+import { useInitialState } from '@/hooks/useInitialState';
+import { useDispatch, useSelector } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as ShopActions from '../controller/action-creators/shop.action-creators'
+import { State } from '@/controller/reducers/root.reducer';
 
 function MyApp(props: AppProps) {
 
@@ -14,6 +18,27 @@ function MyApp(props: AppProps) {
         <Redux {...props} />
       </Provider>
   );
+}
+
+function Redux({ Component, pageProps }: AppProps) {
+
+  const dispatch = useDispatch()
+  const shopActions = bindActionCreators(ShopActions,dispatch)
+  const { shop } = useSelector((state:State) => state)
+  const { appData } = useInitialState()
+
+  useEffect(()=>{
+    shopActions.setProducts(appData)
+  },[appData])
+  
+  return (
+    <div className='main-container'>
+      <Component {...pageProps} redux={{
+        products:shop.products,
+        shopActions
+      }} />
+    </div>
+  )
 }
 
 export default MyApp;
